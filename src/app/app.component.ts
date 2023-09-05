ca858edb87886c76b28de0e2f31f36aed8eaf8ae
Peter Scriven <peter.scriven@trademe.co.nz>
2023-09-06 08:34:17 +1200

    First wiring up of ngForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Subject, tap } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wet-bulb';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  // @ts-ignore
+  @ViewChild('inputs',{static: true}) ngForm: NgForm;
+
+  title = 'wet-bulb'
+  humidity = 15;
+
+  constructor() {
+
+  }
+
+
+  humidity$ = new Subject();
+
+  public ngOnInit(): void {
+    this.humidity$.pipe(
+      tap(v => console.log(`Humidity: ${v}`))
+    )
+  }
+
+  public ngAfterViewInit(): void {
+    // @ts-ignore
+    this.ngForm.valueChanges.subscribe(form => {
+      console.log(form);
+      // this.updated = form;
+    })
+  }
+
 }
